e847c74bc680f1339f7a407198d056b6f9e714ba
Manasask8127 <manasask8127@gmail.com>
2022-02-27 22:07:03 +0530

    coroutines added

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -25,6 +25,10 @@ import com.example.android.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -49,15 +53,25 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue( object: Callback<List<MarsProperty>> {
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                _response.value = "Failure: " + t.message
+       viewModelScope.launch{
+            try{
+                var listResult = MarsApi.retrofitService.getProperties()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            }
+            catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
             }
 
-            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
-            }
-        })
+        }
+//        enqueue( object: Callback<List<MarsProperty>> {
+//            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
+//                _response.value = "Failure: " + t.message
+//            }
+//
+//            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+//                _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
+//            }
+//        })
         _response.value = "Set the Mars API Response here!"
     }
 }
